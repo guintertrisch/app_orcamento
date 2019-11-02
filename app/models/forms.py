9e977606da359d4330024f919aa443d705cba455
Guinter Trisch <guinter.trisch@gmail.com>
2019-11-01 22:30:27 -0300

    ajuste para listar clientes no cadastro de orcamentos

--- app/models/forms.py
+++ app/models/forms.py
@@ -1,10 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField
+from wtforms import StringField, TextAreaField,SelectField
 from wtforms.validators import DataRequired
+from app.models.tables import Cliente
 
 
 class CadOrcamento(FlaskForm):
-    cliente = StringField("cliente",validators=[DataRequired()])
+    #cliente = StringField("cliente",validators=[DataRequired()])
+    cliente = SelectField("Cliente",choices = [(cli.id, cli.nome) for cli in Cliente.query.all()])
     data = StringField("data", validators=[DataRequired()])
     descricao = TextAreaField("descricao", validators=[DataRequired()])
     quantidade = StringField("quantidade", validators=[DataRequired()])
